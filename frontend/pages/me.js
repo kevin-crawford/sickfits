fd3aefb599f8b75ff384e6dfc5b8dd6c9df35b4c
Kevin Crawford <kcdouglas92@gmail.com>
2019-03-12 19:11:39 -0700

    first repo commit

---
+++ frontend/pages/me.js
@@ -0,0 +1,13 @@
+import RequestReset from '../components/RequestReset';
+import styled from 'styled-components';
+import PleaseSignIn from '../components/PleaseSignIn';
+
+const myAccount = props => {
+	<div>
+    <PleaseSignIn>
+      <RequestReset />
+    </PleaseSignIn>
+  </div>
+}
+
+export default myAccount;
